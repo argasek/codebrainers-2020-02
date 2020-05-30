272f25ce6305d58d84e596defe81e266ab9ce60d
bambi <sarnina@me.com>
2020-05-30 16:33:06 +0200

    Add question number prompt to loop

--- src/index.js
+++ src/index.js
@@ -1,30 +1,36 @@
 function question(a, b) {
-  return `How much is ${ a } * ${ b }?`;
+    return `How much is ${a} * ${b}?`;
 }
 
 function drawNumber(scale) {
-  return parseInt(Math.random() * scale) + 1;
+    return parseInt(Math.random() * scale) + 1;
 }
 
-const questionCount = prompt("How many questions do you want to answer?");
+let questionCount;
+
+do {
+    questionCount = prompt("How many questions do you want to answer?");
+
+} while (parseInt(questionCount) === NaN);
+
 let correctAnswerCount = 0;
 
 for (let i = 0; i < questionCount; i++) {
-  const a = drawNumber(10);
-  const b = drawNumber(10);
-  const answer = prompt(question(a, b));
-  if (parseInt(answer) === a * b) {
-    correctAnswerCount++;
-  } else {
-  }
+    const a = drawNumber(10);
+    const b = drawNumber(10);
+    const answer = prompt(question(a, b));
+    if (parseInt(answer) === a * b) {
+        correctAnswerCount++;
+    } else {
+    }
 
-  console.log(question(a, b));
+    console.log(question(a, b));
 }
 
 // undefined
 console.log(correctAnswerCount); //9
 console.log(questionCount); //10
-console.log(correctAnswerCount/questionCount); //0.9
+console.log(correctAnswerCount / questionCount); //0.9
 console.log(0.6 * questionCount); //6
 
 
