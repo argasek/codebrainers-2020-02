1eac15d798d766db11ac4f8327d08d01ae67a6c7
bambi <sarnina@me.com>
2020-05-30 11:53:24 +0200

    Calculus with parameters

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-console.log('Hello world from developers\' console!');
-let userName = prompt("Hello world! What's your name?");
-alert(`Nice to meet you, ${userName}!`);
+
+// // Statements
+// console.log('Hello world from developers\' console!');
+// let userName = prompt("Hello world! What's your name?");
+// alert(`Nice to meet you, ${userName}!`);
+
+function question(a, b) {
+    return `Jaki jest wynik działania, ${a} * ${b}`;
+}
+
+let answer = prompt(question(3, 2));
+
+if (parseInt(answer) === 6) {
+    alert('Well done');
+} else {
+    alert("Wrong answer");
+}
