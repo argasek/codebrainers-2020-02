73b60a349e86bc54bbf1d010e87f81a4557061b9
joannaluciana <core.autocrlf>
2020-05-30 02:53:41 -0700

    Calculus with parameters

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-console.log('Hello world from developers\' console!');
-let userName = prompt("Hello world! What's your name?");
-alert(`Nice to meet you, ${userName}!`);
+//console.log('Hello world from developers\' console!');
+////API web - prompt
+//let userName = prompt("Hello world! What's your name?");
+//alert(`Nice to meet you, ${userName}!`);
+function question (a,b) {
+
+    return `jaki jest wynik działania, ${a} * ${b}?`;
+}
+ let answer = prompt(question(3,2));
+
+ if (parseInt(answer) === 6) {
+ alert('Well done');
+ } else {
+ alert('try again');
+ }
+
+
+
+
+
+
+
+
+
