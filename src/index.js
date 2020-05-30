711971ad4578308bf65a6e38d2af63caf803e3c8
Maksym Garus <bboykermo@gmail.com>
2020-05-30 11:53:29 +0200

    Calculus with parameters

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-console.log('Hello world from developers\' console!');
-let userName = prompt("Hello world! What's your name?");
-alert(`Nice to meet you, ${userName}!`);
+// typy danych w javascripcie : string, number(zmiennoprzecinkowa),
+// boolean np.( var a = true), array (tablice), object
+
+
+// console.log('Hello world from developers\' console!');
+// let userName = prompt("Hello world! What's your name?");
+// alert(`Nice to meet you, ${userName}!`);
+
+// '=' operator przypisania (assignment)
+// '==' operator porównania (comparison)
+// '===' operator identyczności (strict comparison)
+
+// var b = 3;
+// const c = 4;
+// let a = 1.0;
+
+function question(a, b) {
+    return 'Jaki jest wynik działania, ${a} * ${b}';
+}
+
+let answer = prompt(question(3, 2));
+
+if (parseInt(answer) === 6) {
+    alert('Well done!');
+} else {
+    alert('Wrong!');
+}
+console.log()
