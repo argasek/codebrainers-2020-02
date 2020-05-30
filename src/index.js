56ddf88bafbe81b79982d089a43b07987114d9b7
michal Karas <michal.m.karas@gmail.com>
2020-05-30 02:55:38 -0700

    Calculus with parameters

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-console.log('Hello world from developers\' console!');
-let userName = prompt("Hello world! What's your name?");
-alert(`Nice to meet you, ${userName}!`);
+// console.log('Hello world from developers\' console!');
+// let userName = prompt("Hello world! What's your name?");
+// alert(`Nice to meet you, ${userName}!`);
+
+let answer = prompt(question(3, 2));
+if (parseInt(answer) === 6 ) {
+    alert('Well done')
+} else {
+    alert('Try agan')
+}
+
+function question (a, b) {
+    return `jaki jest wynik działania, ${a} * ${b}`;
+}
