1da8a53f7b4c01e5cf19628f47e170152ab0d1a1
Karolina Bochenek <bochenekk@gmail.com>
2020-06-01 06:20:34 -0700

    Add '0 question' option,'asking questions until passed' logic and random operator function

--- src/index.js
+++ src/index.js
@@ -7,8 +7,20 @@
 //    ale również rodzaj działania jaki ma między nimi zajść).
 // 3a*. Dzielenie może być "trochę problematyczne". Znajdź sposób, jak to "trochę" obejść ;)
 
-function question(a, b) {
-    return `How much is ${a} + ${b}?`;
+
+// Dzielenie modulo sporo upraszcza sprawę, ale nie wiem czy to nie zbyt okrężne obejście :D
+
+
+var operators = ['+', '-', '*', '//'];
+
+function getOperator() {
+    return operators[Math.floor(Math.random() * operators.length)];
+}
+
+console.log(getOperator());
+
+function question(a, b, operation) {
+    return `How much is ${a} ${operation} ${b}?`;
 }
 
 function drawNumber(scale) {
@@ -22,21 +34,28 @@ do {
     console.log(questionCount);
 } while (isNaN(questionCount) || questionCount < 0);
 
-let correctAnswerCount = 0;
+if (questionCount === 0) {
+    alert('You get what you give. Your note is also ' + questionCount);
+} else {
+    let correctAnswerCount = 0;
+    let askedQuestionCount = 1;
+    let passed = 0;
 
-for (let i = 0; i < questionCount; i++) {
-    const a = drawNumber(10);
-    const b = drawNumber(10);
-    const answer = prompt(question(a, b));
-    if (parseInt(answer) === a + b) {
-        correctAnswerCount++;
-    } else {
+    while (passed < 0.6 || (askedQuestionCount <= questionCount)) {
+        const a = drawNumber(10);
+        const b = drawNumber(10);
+        const operator = getOperator();
+        const answer = prompt(question(a, b, operator));
+        if (parseInt(answer) === a, operator, b) {
+            correctAnswerCount++;
+        }
+        passed = correctAnswerCount / askedQuestionCount;
+        askedQuestionCount++;
+
+        console.log(askedQuestionCount);
+        console.log(correctAnswerCount);
+        console.log(passed);
     }
 
-    console.log(question(a, b));
+    alert('You passed!'+ '\nNumber of correct answers: ' + correctAnswerCount + '/' + questionCount);
 }
-
-const passed = correctAnswerCount / questionCount >= 0.6;
-
-alert('Passed: ' + passed + '\nNumber of correct answers: ' + correctAnswerCount + '/' + questionCount);
-
