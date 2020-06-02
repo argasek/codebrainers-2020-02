bb4c9fcd4d9ee1685b19ef03dd4b924d629a2fe9
bambi <sarnina@me.com>
2020-06-02 12:57:30 +0200

    Adequate grade to zero questions. WIP: question asking loop makeover.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-// 1. Wystaw użytkownikowi adekwatną ocenę jeśli zdecyduje się na opcję "0 pytań" "no questions asked".
-// 2. Przerób poniższy kod w taki sposób, aby jeśli użytkownik nie zdał, zadawał on (kod!) pytania tak długo,
-//    aż użytkownik zda! Podpowiedź: zamiast wyświetlać okno komunikatu "Passed: false", pomiń wyświetlanie tego
-//    okna i zadawaj pytania tak długo, aż stosunek odpowiedzi poprawnych do niepoprawnych będzie 0.6 do 0.4 ;)
+// 1. Wystaw użytkownikowi adekwatną ocenę jeśli zdecyduje się na opcję "0 pytań" "no questions asked".   -------OKOKOKOKOK
+// 2. Przerób poniższy kod w taki sposób, aby jeśli użytkownik nie zdał, zadawał on (kod!) pytania tak długo, -------OKOKOKOKOK
+//    aż użytkownik zda! Podpowiedź: zamiast wyświetlać okno komunikatu "Passed: false", pomiń wyświetlanie tego  -------OKOKOKOKOK
+//    okna i zadawaj pytania tak długo, aż stosunek odpowiedzi poprawnych do niepoprawnych będzie 0.6 do 0.4 ;)  -------OKOKOKOKOK
 // 3*. Przerób poniższy kod w taki sposób, aby sprawdzał nie tylko znajomość tabliczki mnożenia, ale wszystkich
 //    czterech podstawowych działań, tj. + - / *. (Czyli przy każdym przejściu pętli losował nie tylko 2 liczby,
 //    ale również rodzaj działania jaki ma między nimi zajść).
 // 3a*. Dzielenie może być "trochę problematyczne". Znajdź sposób, jak to "trochę" obejść ;)
+
+//const operators = ['-', '+', '*'];
+
+//let operatorIndex = parseInt(Math.random()) * 3;
+//let randomOperator = operators[operatorIndex];
+
+
 
 function question(a, b) {
     return `How much is ${a} + ${b}?`;
@@ -19,24 +26,24 @@ let questionCount;
 
 do {
     questionCount = parseInt(prompt("How many questions do you want to answer?"));
-    console.log(questionCount);
-} while (isNaN(questionCount) || questionCount < 0);
+    if (questionCount === 0) {
+        alert("0 questions, 0 score");
+    }
+} while (isNaN(questionCount) || questionCount <= 0);
 
 let correctAnswerCount = 0;
 
-for (let i = 0; i < questionCount; i++) {
+do {
     const a = drawNumber(10);
     const b = drawNumber(10);
     const answer = prompt(question(a, b));
+
     if (parseInt(answer) === a + b) {
         correctAnswerCount++;
-    } else {
     }
-
     console.log(question(a, b));
+} while (correctAnswerCount / questionCount < 0.6);
+
+if (correctAnswerCount / questionCount >= 0.6) {
+    alert('Passed');
 }
-
-const passed = correctAnswerCount / questionCount >= 0.6;
-
-alert('Passed: ' + passed + '\nNumber of correct answers: ' + correctAnswerCount + '/' + questionCount);
-
