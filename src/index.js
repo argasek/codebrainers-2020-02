6f1cd97d1e6f2dfe0858e917f4a5e37ee073fe1c
joannaluciana <core.autocrlf>
2020-06-02 03:00:16 -0700

    exe 2 and exe 3 - not working properly

--- src/index.js
+++ src/index.js
@@ -7,10 +7,46 @@
 //    ale również rodzaj działania jaki ma między nimi zajść).
 // 3a*. Dzielenie może być "trochę problematyczne". Znajdź sposób, jak to "trochę" obejść ;)
 
-function question(a, b) {
-    return `How much is ${a} + ${b}?`;
+// function question(a, b) {
+//
+//     return `How much is ${a} + ${b}?`;
+// }
+// function askCalc() {
+const l = parseInt(prompt("What kind of calculation - you need? "));
+// }
+
+function question(a,b) {
+    if (l === 1) {
+        return `How much is ${a} + ${b}?`;
+    }
+    if (l=== 2) {
+        return `How much is ${a} - ${b}?`;
+    }
+    if (l===3) {
+        return `How much is ${a} * ${b}?`;
+    }
+
+    if (l ===4) {
+        return `How much is ${a} / ${b}?`;
+    }
 }
 
+function answerComp() {
+
+    if (l === 1) {
+        return a + b;
+    }
+    if (l === 2) {
+        return  a - b;
+    }
+    if (l ===3) {
+        return a * b;
+    }
+
+    if (l ===4) {
+        return a / b;
+    }
+}
 function drawNumber(scale) {
     return parseInt(Math.random() * scale) + 1;
 }
@@ -20,8 +56,9 @@ let questionCount;
 do {
     questionCount = parseInt(prompt("How many questions do you want to answer?"));
     console.log(questionCount);
-    if (questionCount == 0) {
+    if (questionCount === 0) {
         alert('Chicken!!!');
+
     }
 } while (isNaN(questionCount) || questionCount < 0)
 
@@ -35,13 +72,29 @@ for (let i = 0; i < questionCount; i++) {
     const answer = prompt(question(a, b));
     if (parseInt(answer) === a + b) {
         correctAnswerCount++;
-    } else {
     }
 
     console.log(question(a, b));
 }
 
 const passed = correctAnswerCount / questionCount >= 0.6;
-
 alert('Passed: ' + passed + '\nNumber of correct answers: ' + correctAnswerCount + '/' + questionCount);
+
+
+    do {
+        const a = drawNumber(10);
+        const b = drawNumber(10);
+        const answer = prompt(question(a, b));
+        if (question(a,b) ){
+            correctAnswerCount++;
+        }
+        questionCount++;
+
+        console.log(question(a, b));
+    } while (passed);
+if (passed) {
+    alert('Passed: ' + passed + '\nNumber of correct answers: ' + correctAnswerCount + '/' + questionCount);
+}
+
+
 
