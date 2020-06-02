f12bfb7f033540e5269bb65ff811b6f8e1897012
Jakub Argasiński <jakub@argasinski.eu>
2020-06-02 20:34:37 +0200

    Introduction to arrays and (plain) objects in JavaScript

--- src/index.js
+++ src/index.js
@@ -7,125 +7,54 @@
 // Jeżeli nie, dlaczego? Odpowiedź uzasadnij.
 //
 // Zad. 2
+// Napisz algorytm losujący studenta CodeBrainers to odstrzału!
+
+
+// funkcje -- charakter ogólny, niezwiązany z żadnym obiektem
+// metody = funkcje danego obiektu / klasy
 //
+// zmienne -- charakter ogólny, niezwiązany z żadnym obiektem
+// pola = zmienne danego obiektu / klasy
 
-const PASSING_CRITERIA = 0.6;
+// const a = [
+//   0: [5, 6],
+//   1: "basia",
+//   2: 3,
+//   3: false,
+// ];
 
-let questionCount;
-let correctAnswerCount = 0;
-let a, b;
+// POJO == Plain Old JavaScript Object
 
-function question(a, b, operator) {
-  return `How much is ${ a } ${ operator } ${ b }?`;
+const numbers = [ 13, 666, 2137 ];
+
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i]);
 }
 
-function drawNumber(scale) {
-  return Math.floor(Math.random() * scale) + 1;
-}
+// POJO == Plain Old JavaScript Object
+const students = [
+  {
+    name: 'Jakub Argasiński',
+    age: 18
+  },
+  {
+    name: 'Michał Marchański',
+    age: 13
+  },
+  {
+    name: 'Joanna Lucjanna',
+    age: 24
+  },
+  {
+    name: 'Kaczor Donald',
+    age: 83
+  },
+  {
+    name: 'Grzegorz Lasoń',
+    age: 84
+  },
+];
 
-// 1..4
-function drawOperator(index) {
-  let operatorDrawn;
-  switch (index) {
-    case 1:
-      operatorDrawn = '+';
-      break;
-    case 2:
-      operatorDrawn = '-';
-      break;
-    case 3:
-      operatorDrawn = '*';
-      break;
-    case 4:
-      operatorDrawn = '/';
-      break;
-    default:
-      console.error('Operator needs to have index 1..4');
-  }
-
-  return operatorDrawn;
-}
-
-function drawPair(operator) {
-  let x = drawNumber(10);
-  let y = drawNumber(10);
-
-  if (operator === '+' || operator === '*') {
-    a = x;
-    b = y;
-    return;
-  }
-
-  if (operator === '-') {
-    a = Math.max(x, y);
-    b = Math.min(x, y);
-    return;
-  }
-
-  a = x * y;
-  b = Math.min(x, y);
-}
-
-function calculate(a, b, operator) {
-  switch (operator) {
-    case '+':
-      return a + b;
-    case '-':
-      return a - b;
-    case '*':
-      return a * b;
-    case '/':
-      return a / b;
-    default:
-      console.error('Incorrect operator: ', operator);
-  }
-}
-
-function startAskingQuestions() {
-  let passed = false;
-  let askedQuestionCount = 0;
-  do {
-    const operator = drawOperator(drawNumber(4));
-    drawPair(operator);
-    const answer = prompt(question(a, b, operator));
-
-    console.log(a, operator, b, answer);
-
-    askedQuestionCount++;
-
-    if (parseInt(answer) === calculate(a, b, operator)) {
-      correctAnswerCount++;
-    }
-
-    const exceededOrMetNumberOfRequiredQuestions = askedQuestionCount >= questionCount;
-    const meetAcceptanceCriteria = correctAnswerCount / askedQuestionCount >= PASSING_CRITERIA;
-
-    if (exceededOrMetNumberOfRequiredQuestions && meetAcceptanceCriteria) {
-      passed = true;
-    }
-
-  } while (passed === false);
-
-  alert(
-    'Passed: '
-    + passed
-    + '\nCorrect answers / questions: '
-    + correctAnswerCount + '/'
-    + askedQuestionCount + ' '
-    + 'ratio: '
-    + correctAnswerCount / askedQuestionCount
-  );
-}
-
-do {
-  questionCount = parseInt(prompt("How many questions do you want to answer?"));
-  console.log(questionCount);
-} while (isNaN(questionCount) || questionCount < 0);
-
-// questionCount = 10;
-
-if (questionCount === 0) {
-  alert('Passed: ' + false + '\nNumber of correct answers: well, no questions were asked anyway! ¯\\_(ツ)_/¯');
-} else {
-  startAskingQuestions();
+for (let i = 0; i < students.length; i++) {
+  console.log(students[i]);
 }
