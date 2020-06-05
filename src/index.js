4965e8f27ccce6b7d721af5a0182037407abac34
Karolina Bochenek <bochenekk@gmail.com>
2020-06-05 13:55:22 -0700

    Homework task 1 ONLY solution (copying an array inside an array).

--- src/index.js
+++ src/index.js
@@ -44,14 +44,11 @@ console.log(compareTwoArguments(c, d));
 // Napisz algorytm który będzie kopiował tablicę, która może zawierać tablicę... (Tak aby powstała kopia przez wartość).
 // Skorzystaj z operatora "typeof" i zwróconą przez niego wartość porównaj w instrukcji warunkowej.
 
-// po dłuższym namyśle stwierdzam, że 2* zrobiłam chyba przez przypadek.
-//
-// Task 2*
-// Zmodyfikuj powyższe algorytm tak, aby był w stanie skopiować tablicę która może zawierać tablice które mogą zawierać
-// inne tablice, a one inne tablice... itd. itd.
+// po dłuższym namyśle stwierdzam, że zadanie 2* zrobiłam chyba przez przypadek robiąc zad 1 :)
+// poniżej wersja "tylko task 1"
 
-let testArray = [1, [1, 2, [1, 2, 3], 3], 6, false, 'abc'];
-console.log(testArray[1][2][1]);
+let testArray = [1, [1, 3, [1, 2, 3], 2], 6, false, 'abc'];
+console.log(testArray);
 
 function copyArray(arrayToCopy) {
 
@@ -59,8 +56,13 @@ function copyArray(arrayToCopy) {
     for (let i = 0; i < arrayToCopy.length; i++) {
 
         if (typeof arrayToCopy[i] === 'object') {
-            const copiedItemArray = copyArray(arrayToCopy[i]);
+            let copiedItemArray = [];
+            for (let j = 0; j < arrayToCopy[i].length; j++) {
+                const copiedItem = arrayToCopy[i][j];
+                copiedItemArray.push(copiedItem)
+            }
             copiedArray.push(copiedItemArray);
+
         } else {
             const copiedItem = arrayToCopy[i];
             copiedArray.push(copiedItem);
@@ -71,17 +73,47 @@ function copyArray(arrayToCopy) {
 
 let newArray = copyArray(testArray);
 // newArray[1] = 'bla';
-// newArray[1][0] = 'bla';
-// console.log(newArray[1]);
-// console.log(newArray[1][2]);
-console.log(newArray[1][2][1]);
 newArray[1][2][1] = 'bla';
-console.log(newArray);
-
-// console.log('After copying:')
 console.log(testArray);
-// console.log(typeof testArray[1] === 'object');
-// console.log(copyArray(testArray));
+console.log(newArray);
+//
+//---------------------------------------------
+// Task 2*
+// Zmodyfikuj powyższe algorytm tak, aby był w stanie skopiować tablicę która może zawierać tablice które mogą zawierać
+// inne tablice, a one inne tablice... itd. itd.
+
+// let testArray = [1, [1, 2, [1, 2, 3], 3], 6, false, 'abc'];
+// console.log(testArray[1][2][1]);
+//
+// function copyArray(arrayToCopy) {
+//
+//     let copiedArray = [];
+//     for (let i = 0; i < arrayToCopy.length; i++) {
+//
+//         if (typeof arrayToCopy[i] === 'object') {
+//             const copiedItemArray = copyArray(arrayToCopy[i]);
+//             copiedArray.push(copiedItemArray);
+//         } else {
+//             const copiedItem = arrayToCopy[i];
+//             copiedArray.push(copiedItem);
+//         }
+//     }
+//     return copiedArray;
+// }
+//
+// let newArray = copyArray(testArray);
+// // newArray[1] = 'bla';
+// // newArray[1][0] = 'bla';
+// // console.log(newArray[1]);
+// // console.log(newArray[1][2]);
+// console.log(newArray[1][2][1]);
+// newArray[1][2][1] = 'bla';
+// console.log(newArray);
+//
+// // console.log('After copying:')
+// console.log(testArray);
+// // console.log(typeof testArray[1] === 'object');
+// // console.log(copyArray(testArray));
 
 // -----------------------------------------------------
 // const codeBrainersStudents = [
