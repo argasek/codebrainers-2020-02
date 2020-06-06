469ade262524e24b1923a0396e98cbdcfc93f0eb
michal Karas <michal.m.karas@gmail.com>
2020-06-06 02:46:17 -0700

    Conditional printing of array values

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const arr = [false, true, 665, "false", ["a", "b", 1, [1, 2, 3, 4]], 555];
 
-function print(basia, index, items) {
-    if (items.length % 2 === 0) {
-        console.log(index, basia);
+function print(value, index, items) {
+    if (typeof value === "number") {
+        console.log(index, value);
     }
 }
 
