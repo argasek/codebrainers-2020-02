46144f868e07b88fec395cb536d40039271aa659
bambi <sarnina@me.com>
2020-06-06 16:28:53 +0200

    Implement sorting comparator

--- src/index.js
+++ src/index.js
@@ -20,15 +20,32 @@ function filterOnlyBooleanFields(o) {
     return result;
 }
 
-function objectValues(o){
-    const result=[];
+function objectValues(o) {
+    const result = [];
 
-    for (let key in o){
+    for (let key in o) {
         const value = o[key];
         result.push(value);
     }
     return result;
 }
 
+function alphaSort(a, b) {
+    if (a < b) {
+        return -1;
+    } else if (a > b) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+function sortObjectByKeys(o) {
+    let keysArray = Object.keys(o);
+    return keysArray.sort(alphaSort);
+}
+
 console.log(filterOnlyBooleanFields(car));
 console.log(objectValues(car));
+
+console.log(sortObjectByKeys(car));
