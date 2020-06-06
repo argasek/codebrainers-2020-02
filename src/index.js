d7d45701b3f7101e92d077e84cd41c487152ef97
Grzegorz Lason <lasongrzegorz@gmail.com>
2020-06-06 11:26:35 +0200

    Add conditional iteration

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-const arr = [ false, true, 665, "false", [ "a", "b", 1, [ 1, 2, 3, 4 ] ], 555, 444 ];
+const arr = [false, true, 665, "false", ["a", "b", 1, [1, 2, 3, 4]], 555, 444];
 
 function print(basia, index, items) {
-
+  if (index >= 2) {
     console.log(index, basia);
+  }
 }
 
 arr.forEach(print);
