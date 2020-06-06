31e43699bbeef454afd7224b796e54651f41233f
Maksym Garus <bboykermo@gmail.com>
2020-06-06 14:50:29 +0200

    Test the areAllEven func

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-const studentGrades = [ 1, 2, 3, 4, 5, 2, 2, 3, 2 ];
+const studentGrades = [2, 8, 4, 2, 2, 4, 2];
 
 /**
  * Checks if all items in arr are even.
  * @param arr
  */
 function areAllEven(arr) {
- let result = 0;
+    let result = 0;
 
- arr.forEach(function (value,index, items) {
-    if (value % 2 === 0){
-        result++;
-    }
-
- });
+    arr.forEach(function (value, index, items) {
+        if (value % 2 === 0) {
+            result++;
+        }
+    });
+    return result === arr.length;
 
 }
+
+console.log(areAllEven(studentGrades))
