e3cef70f894cbb8e114ac4a40eacfb8fbe791818
Karolina Bochenek <bochenekk@gmail.com>
2020-06-06 02:10:45 -0700

    Iterators - forEach example

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const arr = [ false, true, 665, "false", [ "a", "b", 1, [ 1, 2, 3, 4 ] ], 555, 444 ];
+
+function print(basia, index, items) {
+
+    console.log(index, basia);
+}
+
+arr.forEach(print);
