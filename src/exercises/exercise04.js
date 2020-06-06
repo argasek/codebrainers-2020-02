928a15191536db95aa7cd7082f91088a98ebe5b8
Michał <m.marchanski@gmail.com>
2020-06-06 16:47:40 +0200

    Add sorting and filtering functions for object

--- src/exercises/exercise04.js
+++ src/exercises/exercise04.js
@@ -1,4 +1,4 @@
-const nestedArray = [ false, true, 665, "false", [ "a", "b", 1, [ 1, 2, 3, 4 ] ], 555 ];
+const nestedArray = [false, true, 665, "false", ["a", "b", 1, [1, 2, 3, 4]], 555];
 
 function printNestedArrayValuesRecursively(value, index, items) {
   if (Array.isArray(value) === true) {
@@ -11,7 +11,7 @@ function printNestedArrayValuesRecursively(value, index, items) {
 
 printNestedArrayValuesRecursively(nestedArray);
 
-const studentGrades = [ 1, 2, 3, 4, 5, 2, 2, 3, 2 ];
+const studentGrades = [1, 2, 3, 4, 5, 2, 2, 3, 2];
 
 let filteringFunction;
 
@@ -41,8 +41,8 @@ console.log(passedStudentsGrades);
 console.log(studentGradesSquared);
 
 
-const isEven = function(value) { return value % 2 === 0; };
-const isOdd = function(grade) { return grade % 2 !== 0; };
+const isEven = function (value) { return value % 2 === 0; };
+const isOdd = function (grade) { return grade % 2 !== 0; };
 
 const areAnyEvenGrades = studentGrades.some(isEven);
 const areAnyOddGrades = studentGrades.some(isOdd);
@@ -56,3 +56,65 @@ console.log('Are there any odd grades?', areAnyOddGrades);
 console.log('Are all even grades?', areAllEvenGrades);
 console.log('Are all odd grades?', areAllOddGrades);
 
+
+// Funkcje: filtrująca pola obiektu, sortująca pola obiektu,
+
+
+const car = {
+  maximumSpeed: 300,
+  isEngineRunning: false,
+  hasRadio: true,
+  isRadioBroken: true,
+  numberOfWheels: 4,
+  gearBox: 'automatic' // 'manual'
+};
+
+function filterOnlyBooleanFields(o) {
+  const result = {};
+
+  for (let key in o) {
+    const value = o[key];
+    if (typeof value === 'boolean') {
+      result[key] = value;
+    }
+  }
+
+  return result;
+}
+
+function objectValues(o) {
+  const result = [];
+
+  for (let key in o) {
+    const value = o[key];
+    result.push(value);
+  }
+  return result;
+}
+
+function alphaSort(a, b) {
+  if (a < b) {
+    return -1;
+  } else if (a > b) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+function sortObjectByKeys(o) {
+  let keysArray = Object.keys(o);
+  const newObject = {};
+  const sortedArrayKeys = keysArray.sort(alphaSort);
+  sortedArrayKeys.forEach(function (item) {
+    console.log(item);
+    const value = o[item];
+    newObject[item] = value;
+  });
+  return newObject;
+}
+
+console.log(filterOnlyBooleanFields(car));
+console.log(objectValues(car));
+
+console.log(sortObjectByKeys(car));
