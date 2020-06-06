0e9aed9f2e05cea396c3cd1d9523fb2240ea0886
Michał <m.marchanski@gmail.com>
2020-06-06 11:56:12 +0200

    Determine if argument is an Array

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const arr = [false, true, 665, "false", ["a", "b", 1, [1, 2, 3, 4]], 555];
 
 function print(value, index, items) {
-    if (typeof value === "number") {
-        console.log(index, value);
+    if (Array.isArray(value) === true) {
+        console.log(index, value, 'is an Array');
+
+    } else {
+        console.log(index, value, 'is not an Array');
     }
 }
+
 
 arr.forEach(print);
