219e20303cc88f22e58ed16953bd908d7b848c93
bambi <sarnina@me.com>
2020-06-09 19:30:42 +0200

    Add bank account class

--- src/exercises/exercise05.js
+++ src/exercises/exercise05.js
@@ -1,14 +1,25 @@
 // ...
+class BankAccount {
+    constructor(accountNumber) {
+        this.accountNumber = accountNumber;
+        this.balance = 0.0;
+        this.debitCardPresent = false;
+    }
+
+}
 
 class Student {
-  constructor(name, age) {
-    this.isPrepared = false;
-    this.bankAccount = undefined;
-    this.name = name;
-    this.age = age;
-    this.albumId = 0;
-  }
+    constructor(name, age) {
+        this.isPrepared = false;
+        this.bankAccount = new BankAccount();
+        this.name = name;
+        this.age = age;
+        this.albumId = 0;
+    }
 }
 
 
 const student = new Student();
+const bankAccount = new BankAccount(23498237492734);
+
+student.bankAccount = bankAccount;
