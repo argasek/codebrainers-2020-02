6975852e98882f7873cd9e23342dff4e15efa6db
Grzegorz Lason <lasongrzegorz@gmail.com>
2020-06-10 17:58:33 +0200

    Add Plant, Room, UserPlant classes

--- src/tasks/exercise-05-task-02.js
+++ src/tasks/exercise-05-task-02.js
@@ -1,10 +1,47 @@
 // Task 2. Create Plant object to reflect data from the API and other API classes from Django.
 // If field in the API is named using snake_case, convert it to camelCase.
 
-class Plant {
-  constructor() {
-  }
+class User {
+  constructor(user) {
+    this.user = user
+  };
 }
 
-// ...
+class Plant {
+  constructor(user, name, description, category, wateringInterval, fertilizingInterval, requiredExposure, requiredHumidity, requiredTemperature, blooming, difficulty) {
+    this.user = new User(user);
+    this.name = name;
+    this.description = description;
+    this.category = category;
+    this.wateringInterval = wateringInterval;
+    this.fertilizingInterval = fertilizingInterval;
+    this.requiredExposure = requiredExposure;
+    this.requiredHumidity = requiredHumidity;
+    this.requiredTemperature = requiredTemperature;
+    this.blooming = blooming;
+    this.difficulty = difficulty;
+  };
+}
 
+class Room {
+  constructor(user, name, description, exposure, humidity, temperature, drafty) {
+    this.user = new User(user);
+    this.name = name;
+    this.description = description;
+    this.exposure = exposure;
+    this.humidity = humidity;
+    this.temperature = temperature;
+    this.drafty = drafty
+  };
+}
+
+class UserPlant {
+  constructor(user, name, description, room, plant, lastFertilized, lastWatered) {
+    this.user = new User(user);
+    this.name = name;
+    this.room = room;
+    this.plant = plant;
+    this.lastFertilized = lastFertilized;
+    this.lastWatered = lastWatered;
+  };
+}
