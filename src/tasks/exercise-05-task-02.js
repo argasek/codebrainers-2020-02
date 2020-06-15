8472c6d4d1e1c3579de5bb4329d29519b14d7948
Karolina Bochenek <bochenekk@gmail.com>
2020-06-15 11:22:54 -0700

    Task 2 solution. Initial implementation of API classes.

--- src/tasks/exercise-05-task-02.js
+++ src/tasks/exercise-05-task-02.js
@@ -1,10 +1,56 @@
 // Task 2. Create Plant object to reflect data from the API and other API classes from Django.
 // If field in the API is named using snake_case, convert it to camelCase.
 
-class Plant {
-  constructor() {
-  }
+class Category {
+    constructor(name) {
+        this.name = name;
+        this.description = '';
+        this.image_url = '';
+        this.user = '';
+    }
 }
 
-// ...
+// const exposureChoices = ['dark', 'shade', 'partsun', 'fullsun'];
 
+
+class Plant {
+    constructor(name, wateringInterval, fertilizingInterval, requiredExposure, requiredHumidity,
+                requiredTemperature, difficulty) {
+        this.name = name;
+        this.description = '';
+        this.user = '';
+        this.category = new Category();  // how to implement one to many relation??
+        this.wateringInterval = wateringInterval;
+        this.fertilizingInterval = fertilizingInterval;
+        this.requiredExposure = requiredExposure;
+        this.requiredHumidity = requiredHumidity;
+        this.requiredTemperature = requiredTemperature;
+        this.blooming = undefined;
+        this.difficulty = difficulty;
+    }
+}
+
+class Room {
+    constructor(name, exposure, humidity, temperature) {
+        this.name = name;
+        this.description = '';
+        this.user = '';
+        this.exposure = exposure;
+        this.humidity = humidity;
+        this.temperature = temperature;
+        this.drafty = undefined;
+    }
+}
+
+class UserPlant {
+    constructor(name) {
+        this.name = name;
+        this.description = '';
+        this.image_url = '';
+        this.user = '';
+        this.room = new Room();
+        this.plant = new Plant();
+        this.lastWatered = undefined;
+        this.lastFertilized = undefined;
+    }
+}
