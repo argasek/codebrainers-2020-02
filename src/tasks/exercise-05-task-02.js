0a81779986f0234238160344ba8ce50a5318724b
bambi <sarnina@me.com>
2020-06-16 16:24:40 +0200

    Task 02, 04 incomplete solution.

--- src/tasks/exercise-05-task-02.js
+++ src/tasks/exercise-05-task-02.js
@@ -1,10 +1,58 @@
 // Task 2. Create Plant object to reflect data from the API and other API classes from Django.
 // If field in the API is named using snake_case, convert it to camelCase.
+// ...
 
-class Plant {
-  constructor() {
+
+class Category {
+  constructor(name) {
+    this.name = name;
+    this.description = '';
+    this.image_url = '';
+    this.user = '';
   }
 }
 
-// ...
 
+class Plant {
+  constructor(name, wateringInterval, fertilizingInterval, requiredExposure, requiredHumidity,
+    requiredTemperature, difficulty) {
+    this.category = [];
+    this.name = name;
+    this.description = '';
+    this.user = '';
+    this.wateringInterval = wateringInterval;
+    this.fertilizingInterval = fertilizingInterval;
+    this.requiredExposure = requiredExposure;
+    this.requiredHumidity = requiredHumidity;
+    this.requiredTemperature = requiredTemperature;
+    this.difficulty = difficulty;
+    this.blooming = undefined;
+  }
+}
+
+
+class Room {
+  constructor(name, exposure, humidity, temperature) {
+    this.name = name;
+    this.description = '';
+    this.user = '';
+    this.exposure = exposure;
+    this.humidity = humidity;
+    this.temperature = temperature;
+    this.drafty = undefined;
+  }
+}
+
+
+class UserPlant {
+  constructor(name) {
+    this.name = name;
+    this.description = '';
+    this.image_url = '';
+    this.user = '';
+    this.room = [];
+    this.plant = [];
+    this.lastWatered = undefined;
+    this.lastFertilized = undefined;
+  }
+}
--- src/tasks/exercise-05-task-04.js
+++ src/tasks/exercise-05-task-04.js
@@ -3,8 +3,47 @@
 // Napisz algorytm, który sprawdza, ile losowań musi się odbyć aby dla zadanego zestawu 6 liczb z zakresu od 1..49
 // (nie powtarzających się) aby trafić "jedynkę", "dwójkę", "trójkę", itd., aż do "szóstki".
 
-const actualNumbers = [ 5, 3, 1, 22, 49, 33 ];
-const expectedNumbers = [ 3, 5, 27, 1, 46, 42 ];
+const actualNumbers = [5, 3, 1, 22, 49, 33];
+const expectedNumbers = [3, 5, 27, 1, 46, 42];
 
 
+let numbers = [];
 
+for (i = 0; i < 49; i++) {
+    numbers[i] = i + 1;
+}
+
+function shuffle(array) {
+    let tmp, current, top = array.length;
+    if (top) while (--top) {
+        current = Math.floor(Math.random() * (top + 1));
+        tmp = array[current];
+        array[current] = array[top];
+        array[top] = tmp;
+    }
+    return array;
+}
+
+let drawnNumbers = [];
+
+for (i = 0; i < 6; i++) {
+    drawnNumbers.push(shuffle(numbers)[i])
+}
+console.log(drawnNumbers)
+
+
+let drawLots = 0;
+
+// function lotto(bet, num) {
+
+//     for (i = 0; i < 100; i++) {
+//         if (num !== bet) {
+//             drawLots = drawLots + 1;
+//         }
+
+//         return drawLots;
+//     }
+// }
+
+// // console.log(drawLots)
+// console.log(lotto(expectedNumbers, drawnNumbers));
