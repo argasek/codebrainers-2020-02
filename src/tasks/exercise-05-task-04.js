7425c39f173cfc7eea1693b1761db8eb6946affd
Karolina Bochenek <bochenekk@gmail.com>
2020-06-16 07:53:11 -0700

    Task 4 solution, step 1. Add functions drawNumberSet and countCommonNumbers

--- src/tasks/exercise-05-task-04.js
+++ src/tasks/exercise-05-task-04.js
@@ -3,8 +3,57 @@
 // Napisz algorytm, który sprawdza, ile losowań musi się odbyć aby dla zadanego zestawu 6 liczb z zakresu od 1..49
 // (nie powtarzających się) aby trafić "jedynkę", "dwójkę", "trójkę", itd., aż do "szóstki".
 
-const actualNumbers = [ 5, 3, 1, 22, 49, 33 ];
-const expectedNumbers = [ 3, 5, 27, 1, 46, 42 ];
+const actualNumbers = [5, 3, 1, 22, 49, 33];
+const expectedNumbers = [3, 5, 27, 1, 46, 42];
 
+
+function alphaSort(a, b) {
+    return a - b;
+}
+
+
+function drawNumbers() {
+
+    const availableNumbersSet = [];
+    for (let i = 1; i <= 49; i++) {
+        availableNumbersSet.push(i);
+    }
+
+    // console.log(availableNumbersSet);
+
+    let scale = 49;
+    let newNumbersSet = [];
+    for (let i = 0; i < 6; i++) {
+        let newNumberIndex = Math.floor(Math.random() * availableNumbersSet.length);
+
+        newNumbersSet.push(availableNumbersSet[newNumberIndex]);
+        // console.log(newNumberIndex, availableNumbersSet.length);
+        availableNumbersSet.splice(newNumberIndex, 1);
+        // console.log(availableNumbersSet.length);
+    }
+    return newNumbersSet;
+}
+
+// console.log(drawNumbers());
+
+
+function countCommonNumbers(set1, set2) {
+    set1.sort(alphaSort);
+    set2.sort(alphaSort);
+    let commonNumbersCount = 0;
+
+    set1.forEach(function (value1) {
+        set2.some(function (value2) {
+            if (value2 === value1) {
+                commonNumbersCount++;
+                // console.log(value2);
+            }
+        })
+    })
+    return commonNumbersCount;
+}
+
+// console.log("How many numbers are matching both sets?");
+// console.log(countCommonNumbers(actualNumbers, expectedNumbers));
 
 
