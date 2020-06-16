9f6b07d3c32138a76d4493ab85242db97d0cedad
Maksym Garus <bboykermo@gmail.com>
2020-06-16 16:06:49 +0200

    Task 2 solution

--- src/tasks/exercise-05-task-02.js
+++ src/tasks/exercise-05-task-02.js
@@ -2,9 +2,22 @@
 // If field in the API is named using snake_case, convert it to camelCase.
 
 class Plant {
-  constructor() {
+  static id = 0;
+
+  constructor(name, category) {
+    Plant.id++
+    this.id = Plant.id;
+    this.name = name;
+    this.description = '';
+    this.category = category;
+    this.watering_interval = 0;
+    this.fertilizing_interval = 0;
+    this.required_exposure = '';
+    this.required_humidity = '';
+    this.required_temperature = '';
+    this.blooming = false;
+    this.difficulty = '';
+    this.user = '';
+    this.url = '';
   }
 }
-
-// ...
-
