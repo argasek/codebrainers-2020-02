ed77bbee648a286bb5f1000868980206b87eccb2
Jakub Argasiński <jakub@argasinski.eu>
2020-06-19 21:07:34 +0200

    Exercise 07 - introduction to arrow functions notation.

--- src/components/randomNumberGenerator.js
+++ src/components/randomNumberGenerator.js
@@ -1,5 +1,20 @@
-const getRandomNumber = () => {
+// function getRandomNumberClassicFunction() {
+//   return Math.random();
+// }
+//
+// const getRandomNumberArrowFunction = () => {
+//   return Math.random();
+// }
+//
 
-    return Math.random();
+const getRandomNumber = () => Math.random();
 
-}
+let b = [ 3, 4, 5 ]
+  .map(x => x * x)
+  .filter(x => x > 16)
+  .reduce((prev, current) => prev + current, 0);
+
+console.log(b);
+
+
+
