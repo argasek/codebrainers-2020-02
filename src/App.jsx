74614f6e8b10127658b546d93c0f4bd5ec47479b
joannaluciana <core.autocrlf>
2020-06-20 02:48:54 -0700

    Add App styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './App.css';
 import StudentList from 'components/student-list/StudentList';
 
 function App() {
     return (
-        <div>
+        <div className="app">
             <h1>Hello world!</h1>
             <StudentList/>
         </div>
