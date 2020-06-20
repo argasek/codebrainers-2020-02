f24048a7b224a666932c9f7b3b15302ed4b1a5f4
Jakub Argasiński <jakub@argasinski.eu>
2020-06-20 16:57:15 +0200

    Exercise 08 - Add homework tasks

--- src/components/student-list/StudentList.jsx
+++ src/components/student-list/StudentList.jsx
@@ -7,11 +7,11 @@ const sortStudents = (studentsArray, key, direction) => {
   const counter = direction === "desc" ? -1 : 1;
   return studentsArray.sort(function (a, b) {
     if (a[key] < b[key]) {
-      return -1;
+      return -1 * counter;
     } else if (a[key] === b[key]) {
       return 0;
     } else if (a[key] > b[key]) {
-      return 1;
+      return 1 * counter;
     }
   });
 }
@@ -22,13 +22,13 @@ const StudentList = () => {
     <table className='student-list-table'>
       <thead>
         <tr>
-          <th className='student-header student-cell-padding'>student.name</th>
-          <th className='student-header student-cell-padding'>Coffees</th>
+          <th className='student-header student-cell-padding'>Student name</th>
+          <th className='student-header student-cell-padding'>☕</th>
         </tr>
       </thead>
       <tbody>
         {
-          sortStudents(codeBrainersStudents, "coffees", "desc").map(student =>
+          sortStudents(codeBrainersStudents, "coffees", "asc").map(student =>
             <Student
               key={student.id}
               student={student}
---
+++ src/tasks/exercise-08-tasks.js
@@ -0,0 +1,49 @@
+/*
+
+ZAD. 1 -- Z "GWIAZDKĄ" ;) (trudne)
+======================================================================
+
+1. Przerób funkcję sortStudents() w taki sposób, aby można było sortować po dwóch parametrach i kierunkach.
+
+Np. Jeśli powiem, że chcę posortować po kawach rosnąco a po imionach malejąco, to powinienem uzyskać taki efekt:
+
+
+ |  0 Kamila     ^
+ |  0 Grzegorz   |
+ |  1 Michał M.    ^
+ |  1 Michał K.    |
+ |  1 Damian       |
+ |  2 Sabina        ^
+ |  2 Maksym        |
+ |  2 Joanna        |
+ |  3 Karolina        ^
+\|/
+
+(1) najpierw po ilości kaw rosnąco
+(2) potem po nazwiskach malejąco
+
+
+ZAD. 2 (proste)
+======================================================================
+
+Uprość funkcję sortStudents() tak aby była krótsza niż 9 wierszy.
+
+ZAD. 3 (proste)
+======================================================================
+
+Zaimplementuj funkcję randomizeStudents(), która będzie układać studentów w losowej kolejności.
+
+ZAD. 4 (też proste)
+======================================================================
+
+Zmodyfikuj kod w taki sposób, aby sortowanie studentów było aplikowane tylko do drugiej listy.
+Pierwsza lista powinna zachowywać kolejność podaną w modelu.
+
+ZAD. 5* (trudne)
+======================================================================
+Zmodyfikuj kod w taki posób, żeby kliknięcie w przycisk "Randomize students" układało studentów
+w losowej kolejności każdorazowo gdy przycisk zostanie kliknięty.
+
+ */
+
+
