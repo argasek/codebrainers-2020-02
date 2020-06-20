f27c0fc36ad815c416319080d7355570bfb6ec70
bambi <sarnina@me.com>
2020-06-20 10:15:48 +0200

    Iterate over student list.

--- src/components/student-list/StudentList.jsx
+++ src/components/student-list/StudentList.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 import codeBrainersStudents from '../../models/Students';
 
 const StudentList = () => {
-    const test = 'Testowy string';
-    return (
-      <div>
-          <p>{ test }</p>
-      </div>
-    );
+  const test = codeBrainersStudents[0].name;
+  // arr.forEach(callback[, thisArg])
+
+  const studentNames = codeBrainersStudents.map(student => { return student.name; });
+
+
+
+
+
+  return (
+    <div>
+      <p>{studentNames}</p>
+    </div>
+  );
 };
 
 export default StudentList;
