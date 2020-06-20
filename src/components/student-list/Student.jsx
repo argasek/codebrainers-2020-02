d45af8551b7e7e9bd889e598003f69cf9dc4a6fb
Jakub Argasiński <jakub@argasinski.eu>
2020-06-20 14:03:48 +0200

    Exercise 08 - visual formatting of a component based on variable values

--- src/components/student-list/Student.jsx
+++ src/components/student-list/Student.jsx
@@ -1,26 +1,35 @@
 import React from 'react';
 import './Student.css';
 
-
-
 const Student = (props) => {
-    const studentName = props.studentName;
-    const coffees = props.studentCoffees;
-    const divStyle = {
-      paddingLeft: coffees * 5 + 'em',
-        backgroundColor: coffees===0 ? 'red':'grey'
-    };
+  const studentName = props.studentName;
+  const coffees = props.studentCoffees;
 
-    return (
-        <tr>
-            <td>
-              <div style={divStyle} className="student">
-              {studentName}
-              </div>
-            </td>
-            <td>{coffees}</td>
-        </tr>
-    );
+  function limitRange(x, min, max) {
+    return Math.min(Math.max(Math.round(x), min), max)
+  }
+
+  /**
+   * @param {number} coffees Number of coffees
+   * @return {string} Returns string like 'coffee0', 'coffee1', …, 'coffee10'
+   */
+  function getCoffeeClassName(coffees) {
+    coffees = limitRange(coffees, 0, 10);
+    return 'coffee' + coffees;
+  }
+
+  const className = 'student' + ' ' + getCoffeeClassName(coffees);
+
+  return (
+    <tr>
+      <td>
+        <div className={ className } >
+          { studentName }
+        </div>
+      </td>
+      <td>{ coffees }</td>
+    </tr>
+  );
 };
 
 
