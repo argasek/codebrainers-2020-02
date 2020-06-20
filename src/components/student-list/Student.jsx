fd40929f5c0152d4e11ae3fa4652116455c8d72e
michal Karas <michal.m.karas@gmail.com>
2020-06-20 02:35:08 -0700

    Display list of students as table

--- src/components/student-list/Student.jsx
+++ src/components/student-list/Student.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
 const Student = (props) => {
-  const studentName = props.studentName;
-  const coffees = props.studentCoffees;
-  return (
-    <li>{ studentName } { coffees }</li>
-  );
+    const studentName = props.studentName;
+    const coffees = props.studentCoffees;
+    return (
+        <tr>
+            <td>{studentName}</td>
+            <td>{coffees}</td>
+        </tr>
+    );
 };
-
 
 
 export default Student;
--- src/components/student-list/StudentList.jsx
+++ src/components/student-list/StudentList.jsx
@@ -4,15 +4,45 @@ import Student from 'components/student-list/Student';
 
 const StudentList = () => {
 
-  return (
-    <ul>
-      {
-        codeBrainersStudents.map(student =>
-          <Student key={ student.id } studentName={ student.name } studentCoffees={student.coffees}/>
-        )
-      }
-    </ul>
-  );
+    return (
+        /*
+                <table>
+                    <thead>
+                        <tr>
+                            <th colSpan="2">The table header</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>The table body</td>
+                            <td>with two columns</td>
+                        </tr>
+                    </tbody>
+                </table>
+        */
+
+        <table>
+            <thead>
+            <tr>
+                <th>Name</th>
+                <th>Coffees</th>
+            </tr>
+            </thead>
+            <tbody>
+                {
+                    codeBrainersStudents.map(student =>
+                        <Student
+                            key={student.id}
+                            studentName={student.name}
+                            studentCoffees={student.coffees}
+                        />
+                    )
+                }
+            </tbody>
+        </table>
+    );
 };
 
 export default StudentList;
+
+
