b63d04cee9db6ee3b7671eb394dd49cf76e64a48
Maksym Garus <bboykermo@gmail.com>
2020-06-20 15:56:30 +0200

    Sort students by name

--- src/components/student-list/StudentList.jsx
+++ src/components/student-list/StudentList.jsx
@@ -3,26 +3,37 @@ import codeBrainersStudents from '../../models/Students';
 import Student from 'components/student/Student';
 import './StudentList.css';
 
+const sortStudents = (studentsArray) => {
+  return studentsArray.sort(function (a, b) {
+    if (a.name < b.name) {
+      return -1;
+    } else if (a.name === b.name) {
+      return 0;
+    } else if (a.name > b.name) {
+      return 1;
+    }
+  });
+}
 const StudentList = () => {
 
   return (
 
     <table className='student-list-table'>
       <thead>
-      <tr>
-        <th className='student-header student-cell-padding'>Name</th>
-        <th className='student-header student-cell-padding'>Coffees</th>
-      </tr>
+        <tr>
+          <th className='student-header student-cell-padding'>student.name</th>
+          <th className='student-header student-cell-padding'>Coffees</th>
+        </tr>
       </thead>
       <tbody>
-      {
-        codeBrainersStudents.map(student =>
-          <Student
-            key={ student.id }
-            student={ student }
-          />
-        )
-      }
+        {
+          sortStudents(codeBrainersStudents).map(student =>
+            <Student
+              key={student.id}
+              student={student}
+            />
+          )
+        }
       </tbody>
     </table>
   );
