cff3ef5f4b2d6cfcfe7404b38fd3ca67647be552
Jakub Argasiński <jakub@argasinski.eu>
2020-06-20 12:20:13 +0200

    Exercise 08 - two ways of styling components - className and style props.

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css';
 import StudentList from 'components/student-list/StudentList';
 
 function App() {
-    return (
-        <div className="app">
-            <h1>Hello world!</h1>
-            <StudentList/>
-        </div>
-    );
+  return (
+    <div className="app">
+      <h1>m Hello world!</h1>
+      <h1 className="small">m Hello world!</h1>
+      <p>m</p>
+      <StudentList />
+    </div>
+  );
 }
 
 export default App;
--- src/components/student-list/Student.jsx
+++ src/components/student-list/Student.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import './Student.css';
+
+
 
 const Student = (props) => {
     const studentName = props.studentName;
     const coffees = props.studentCoffees;
+    const divStyle = {
+      paddingLeft: coffees * 5 + 'em',
+    };
+
     return (
         <tr>
-            <td>{studentName}</td>
+            <td>
+              <div style={divStyle} className="student">
+              {studentName}
+              </div>
+            </td>
             <td>{coffees}</td>
         </tr>
     );
