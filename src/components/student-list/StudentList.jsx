057b90d8f78d1b498d0f0f7553c20a82d1b2b3b3
Karolina Bochenek <bochenekk@gmail.com>
2020-06-21 06:43:29 -0700

    Task 4 solution: use sortStudents function only for the second table of students.

--- src/components/student-list/StudentList.jsx
+++ src/components/student-list/StudentList.jsx
@@ -3,18 +3,18 @@ import codeBrainersStudents from '../../models/Students';
 import Student from 'components/student/Student';
 import './StudentList.css';
 
-// const sortStudents = (studentsArray, key, direction) => {
-//   const counter = direction === "desc" ? -1 : 1;
-//   return studentsArray.sort(function (a, b) {
-//     if (a[key] < b[key]) {
-//       return -1 * counter;
-//     } else if (a[key] === b[key]) {
-//       return 0;
-//     } else if (a[key] > b[key]) {
-//       return 1 * counter;
-//     }
-//   });
-// }
+const sortStudents = (studentsArray, key, direction) => {
+  const counter = direction === "desc" ? -1 : 1;
+  return studentsArray.sort(function (a, b) {
+    if (a[key] < b[key]) {
+      return -1 * counter;
+    } else if (a[key] === b[key]) {
+      return 0;
+    } else if (a[key] > b[key]) {
+      return 1 * counter;
+    }
+  });
+}
 
 function copyArray(arr) {
   const newArray = [];
@@ -32,7 +32,6 @@ const randomizeStudents = (studentsArray) => {
 
   for (let i = 0; i < studentsArray.length; i++) {
     const randomIndex = Math.floor(Math.random() * temporaryArray.length);
-    // let student = temporaryArray[randomIndex];
     shuffledArray.push(temporaryArray[randomIndex]);
     temporaryArray.splice(randomIndex, 1);
   }
@@ -53,7 +52,7 @@ const StudentList = () => {
       </thead>
       <tbody>
       {
-        randomizeStudents(codeBrainersStudents, "coffees", "asc").map(student =>
+        codeBrainersStudents.map(student =>
           <Student
             key={student.id}
             student={student}
@@ -65,6 +64,56 @@ const StudentList = () => {
   );
 };
 
-export default StudentList;
+const SortedStudentList = () => {
+
+  return (
+
+    <table className='student-list-table'>
+      <thead>
+      <tr>
+        <th className='student-header student-cell-padding'>Student name</th>
+        <th className='student-header student-cell-padding'>☕</th>
+      </tr>
+      </thead>
+      <tbody>
+      {
+        sortStudents(codeBrainersStudents, "coffees", "asc").map(student =>
+          <Student
+            key={student.id}
+            student={student}
+          />
+        )
+      }
+      </tbody>
+    </table>
+  );
+};
+
+// const RandomizeStudentList = () => {
+//
+//   return (
+//
+//     <table className='student-list-table'>
+//       <thead>
+//       <tr>
+//         <th className='student-header student-cell-padding'>Student name</th>
+//         <th className='student-header student-cell-padding'>☕</th>
+//       </tr>
+//       </thead>
+//       <tbody>
+//       {
+//         randomizeStudents(codeBrainersStudents, "coffees", "asc").map(student =>
+//           <Student
+//             key={student.id}
+//             student={student}
+//           />
+//         )
+//       }
+//       </tbody>
+//     </table>
+//   );
+// };
+
+export {StudentList, SortedStudentList};
 
 
