8cfb09ecf7dbc840aac30348f8c2d442db1b57b1
Grzegorz Lason <lasongrzegorz@gmail.com>
2020-06-21 14:11:27 +0200

    Add sort oneliner, randomize function, studentList org and modified separation

--- src/components/student-list/StudentList.jsx
+++ src/components/student-list/StudentList.jsx
@@ -6,40 +6,78 @@ import './StudentList.css';
 const sortStudents = (studentsArray, key, direction) => {
   const counter = direction === "desc" ? -1 : 1;
   return studentsArray.sort(function (a, b) {
-    if (a[key] < b[key]) {
-      return -1 * counter;
-    } else if (a[key] === b[key]) {
-      return 0;
-    } else if (a[key] > b[key]) {
-      return 1 * counter;
-    }
+    return a[key] < b[key] ? -1 * counter : a[key] === b[key] ? 0 * counter : 1 * counter
+    // if (a[key] < b[key]) {
+    //   return -1 * counter;
+    // } else if (a[key] === b[key]) {
+    //   return 0;
+    // } else if (a[key] > b[key]) {
+    //   return 1 * counter;
+    // }
   });
 }
-const StudentList = () => {
+
+const randomizeStudents = (studentsArray) => {
+  return studentsArray.sort(() => Math.random() - 0.5);
+}
+
+const StudentListOriginal = () => {
 
   return (
 
     <table className='student-list-table'>
       <thead>
-        <tr>
-          <th className='student-header student-cell-padding'>Student name</th>
-          <th className='student-header student-cell-padding'>☕</th>
-        </tr>
+      <tr>
+        <th className='student-header student-cell-padding'>Student name</th>
+        <th className='student-header student-cell-padding'>☕</th>
+      </tr>
       </thead>
       <tbody>
-        {
-          sortStudents(codeBrainersStudents, "coffees", "asc").map(student =>
-            <Student
-              key={student.id}
-              student={student}
-            />
-          )
-        }
+      {
+        codeBrainersStudents.map(student =>
+          <Student
+            key={student.id}
+            student={student}
+          />
+        )
+      }
       </tbody>
     </table>
   );
 };
 
-export default StudentList;
+export default StudentListOriginal;
+
+export const StudentListRearranged = () => {
+
+  return (
+
+    <table className='student-list-table'>
+      <thead>
+      <tr>
+        <th className='student-header student-cell-padding'>Student name</th>
+        <th className='student-header student-cell-padding'>☕</th>
+      </tr>
+      </thead>
+      <tbody>
+      {
+        randomizeStudents(codeBrainersStudents).map(student =>
+          <Student
+            key={student.id}
+            student={student}
+          />
+        )
+        // sortStudents(codeBrainersStudents, "coffees", "desc").map(student =>
+        //   <Student
+        //     key={student.id}
+        //     student={student}
+        //   />
+        // )
+      }
+      </tbody>
+    </table>
+  );
+};
+
 
 
