15ec856e2654941dc4b0a09b35c96f8ecca114ce
Karolina Bochenek <bochenekk@gmail.com>
2020-07-02 11:37:20 -0700

    Implement table to display Plants.

--- src/components/plants/Plants.jsx
+++ src/components/plants/Plants.jsx
@@ -1,9 +1,10 @@
-import { Card, CardBody } from "reactstrap";
+import {Card, CardBody} from "reactstrap";
 import React from "react";
 import PropTypes from "prop-types";
 import axios from "axios";
 import Plant from "components/plants/Plant";
 import InProgress from "components/shared/InProgress";
+import {Table} from 'reactstrap';
 
 const PLANTS_FETCH_DELAY = 250;
 
@@ -19,13 +20,13 @@ class Plants extends React.PureComponent {
 
   componentDidMount() {
     this.fetchPlants().finally(() => {
-      this.setState({ inProgress: false });
+      this.setState({inProgress: false});
     });
   }
 
   fetchPlants() {
     const requestUrl = "http://gentle-tor-07382.herokuapp.com/plants/";
-    this.setState({ inProgress: true });
+    this.setState({inProgress: true});
     return this.props.delayFetch(PLANTS_FETCH_DELAY, (resolve, reject) => {
       axios
         .get(requestUrl)
@@ -33,30 +34,48 @@ class Plants extends React.PureComponent {
           const data = response.data;
           const plants = data.map((item) => item.name);
           const successPlants = true;
-          this.setState({ plants, successPlants });
+          this.setState({plants, successPlants});
           resolve();
         })
         .catch((error) => {
-          this.setState({ successPlants: false });
+          this.setState({successPlants: false});
           reject();
         });
     });
   }
 
   render() {
-    const { plants, successPlants, inProgress } = this.state;
+    const {plants, successPlants, inProgress} = this.state;
 
     return (
       <Card className="mb-4">
         <CardBody>
-          <InProgress inProgress={inProgress} />
+          <InProgress inProgress={inProgress}/>
           {successPlants === false && <p>Nie udało się pobrać Kwiatow</p>}
           {successPlants && (
-            <div className="plants">
+            <Table striped>
+              <thead>
+              <tr>
+                <th>#</th>
+                <th>Plant Name</th>
+                <th>Last Name</th>
+                <th>Username</th>
+              </tr>
+              </thead>
+              <tbody>
+
               {plants.map((plant, index, arr) => (
-                <Plant name={plant} key={index} />
+                <tr>
+                  <th scope="row">1</th>
+                  <td>{plant}</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                </tr>
+
               ))}
-            </div>
+              </tbody>
+
+            </Table>
           )}
         </CardBody>
       </Card>
